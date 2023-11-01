2aa27f4a032adc2237cdcf642b66b406c15faaa0
Robert Stapleton <robert@stapleton-family.net>
2023-11-01 14:12:22 -0400

    added system free space on object

--- Server.Report.ps1
+++ Server.Report.ps1
@@ -4,9 +4,13 @@ $ComputerInfo = Get-ComputerInfo
 # get disk info
 $DiskInfo = Get-Disk
 
+# get free system space
+$FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
+
 $obj = [PSCustomObject]@{
     MachineName = $ComputerInfo.CsDNSHostName
     OSVersion = $ComputerInfo.OsVersion
     LogicalDiskCount = $DiskInfo.count
+    FreeSpace = $FreeSpace.Free
 }
 Write-Output $obj
