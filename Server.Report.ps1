b52b9462232bf7ed5c2aa17fa3b4beba5a110598
Robert Stapleton <robert@stapleton-family.net>
2023-11-01 15:05:26 -0400

    added cpu core count to object

--- Server.Report.ps1
+++ Server.Report.ps1
@@ -7,10 +7,14 @@ $DiskInfo = Get-Disk
 # get free system space
 $FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
 
+# get number of cores
+$CPUCore = Get-CimInstance CIM_Processor
+
 $obj = [PSCustomObject]@{
     MachineName = $ComputerInfo.CsDNSHostName
     OSVersion = $ComputerInfo.OsVersion
     LogicalDiskCount = $DiskInfo.count
     FreeSpace_GB = [System.Math]::Round(($FreeSpace.Free / 1gb),1)
+    CPUCore = $CPUCore.NumberOfCores
 }
 Write-Output $obj
