025ad6adccf9cf5c2a41f305af6b6672772f284a
Robert Stapleton <robert@stapleton-family.net>
2023-11-01 14:40:46 -0400

    formatted free space value

--- Server.Report.ps1
+++ Server.Report.ps1
@@ -11,6 +11,6 @@ $obj = [PSCustomObject]@{
     MachineName = $ComputerInfo.CsDNSHostName
     OSVersion = $ComputerInfo.OsVersion
     LogicalDiskCount = $DiskInfo.count
-    'FreeSpace_GB' = $FreeSpace.Free / 1gb
+    FreeSpace_GB = [System.Math]::Round(($FreeSpace.Free / 1gb),1)
 }
 Write-Output $obj
