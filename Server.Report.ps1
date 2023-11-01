76cf99d63982a40dccb1ff90d6b4847ace99f300
Robert Stapleton <robert@stapleton-family.net>
2023-11-01 15:27:12 -0400

    added total memory to obj

--- Server.Report.ps1
+++ Server.Report.ps1
@@ -5,16 +5,24 @@ $ComputerInfo = Get-ComputerInfo
 $DiskInfo = Get-Disk
 
 # get free system space
-$FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
+$FreeSpace = Get-PSDrive | Where-Object { $_.Name -eq 'C' }
 
 # get number of cores
 $CPUCore = Get-CimInstance CIM_Processor
 
+# get total memory capacity
+$Memory = Get-CimInstance CIM_PhysicalMemory
+$MemorySum = 0
+foreach ($item in $Memory) {
+    $MemorySum += $item.Capacity
+}
+ 
 $obj = [PSCustomObject]@{
-    MachineName = $ComputerInfo.CsDNSHostName
-    OSVersion = $ComputerInfo.OsVersion
+    MachineName      = $ComputerInfo.CsDNSHostName
+    OSVersion        = $ComputerInfo.OsVersion
     LogicalDiskCount = $DiskInfo.count
-    FreeSpace_GB = [System.Math]::Round(($FreeSpace.Free / 1gb),1)
-    CPUCore = $CPUCore.NumberOfCores
+    FreeSpace_GB     = [System.Math]::Round(($FreeSpace.Free / 1gb), 1)
+    CPUCore          = $CPUCore.NumberOfCores
+    TotalMemory      = $MemorySum / 1gb
 }
 Write-Output $obj
